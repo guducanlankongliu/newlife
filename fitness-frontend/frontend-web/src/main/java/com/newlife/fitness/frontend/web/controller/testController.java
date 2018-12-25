2d4b5cb0138a702cdd499ba2ec2c3d5d2d40e32f
liuyuanpeng <2015554867@qq.com>
2018-12-25 22:52:50 +0800

    日常测试前端没什么问题

--- fitness-frontend/frontend-web/src/main/java/com/newlife/fitness/frontend/web/controller/testController.java
+++ fitness-frontend/frontend-web/src/main/java/com/newlife/fitness/frontend/web/controller/testController.java
@@ -1,4 +1,4 @@
-package com.newlife.fitness.frontend.web;
+package com.newlife.fitness.frontend.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
